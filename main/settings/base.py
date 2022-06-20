35868a27dbda270a9422e68feba520cbcfe3e678
fizyomustafa <fizyomustafa@gmail.com>
2022-06-20 23:18:02 +0300

    template drf

--- main/settings/base.py
+++ main/settings/base.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +44,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'main.urls'
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -63,15 +65,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -114,3 +107,53 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#LOGGER yani basılan hataları falan tutan yerin kodu, buradan aşağıyı kopyala al Debugları bir yere toplamak istiyorsan.
+
+LOGGING = {
+    "version": 1,
+    # is set to True then all loggers from the default configuration will be disabled.
+    "disable_existing_loggers": True,
+    # Formatters describe the exact format of that text of a log record. 
+    "formatters": {
+        "standard": {
+            "format": "[%(levelname)s] %(asctime)s %(name)s: %(message)s"
+        },
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    # The handler is the engine that determines what happens to each message in a logger.
+    # It describes a particular logging behavior, such as writing a message to the screen, 
+    # to a file, or to a network socket.
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "level": "INFO",
+            "stream": "ext://sys.stdout",
+            },
+        'file': {
+            'class': 'logging.FileHandler',
+            "formatter": "verbose",
+            'filename': './debug.log',
+            'level': 'INFO', #buraya hani seviyede hataları istiyorsak onları yazabiliriz. WARNING, ERROR, CRITICAL, INFO, DEBUG olabilir.
+        },
+    },
+    # A logger is the entry point into the logging system.
+    "loggers": {
+        "django": {
+            "handlers": ["console", 'file'],
+            # log level describes the severity of the messages that the logger will handle. 
+            "level": config("DJANGO_LOG_LEVEL", "INFO"),
+            'propagate': True,
+            # If False, this means that log messages written to django.request 
+            # will not be handled by the django logger.
+        },
+    },
+}
